8ff1f78282593a11097efd064ae69905f30d075e
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2019-12-17 20:45:20 +0100

    iteration 1

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -7,13 +7,17 @@ const User = require('../models/user.js');
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
+const uploadCloud=require("../config/cloudinary.js");
+
 router.get('/signup', (req, res) => {
   res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post("/signup", (req, res, next) => {
+router.post("/signup",  uploadCloud.single("profilepic"), (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
+  const imgPath=req.file.url;
+  const imgName= req.file.originalname;
 
   // 1. Check username and password are not empty
   if (username === "" || password === "") {
@@ -39,7 +43,9 @@ router.post("/signup", (req, res, next) => {
 
       const newUser = new User({
         username,
-        password: hashPass
+        password: hashPass,
+        imgName,
+        imgPath,
       });
 
       newUser.save()
