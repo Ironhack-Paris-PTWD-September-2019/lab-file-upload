d6a59a80f57a3f8cf0a1a35e0547fb83f5427dec
Thao Nhu <thaonhu2710@yahoo.com>
2019-12-17 21:48:02 +0100

    Middle of iteration2

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const passport = require('passport');
 const User = require('../models/user.js');
+const Post = require('../models/post.js');
+const uploadCloud = require('../config/cloudinary.js');
 
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
@@ -11,9 +13,12 @@ router.get('/signup', (req, res) => {
   res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post("/signup", (req, res, next) => {
+router.post("/signup", uploadCloud.single("userphoto"),(req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
+  const email = req.body.email;
+  const imgPath = req.file.url;
+  const imgName = req.file.originalname;
 
   // 1. Check username and password are not empty
   if (username === "" || password === "") {
@@ -39,7 +44,10 @@ router.post("/signup", (req, res, next) => {
 
       const newUser = new User({
         username,
-        password: hashPass
+        password: hashPass,
+        email,
+        imgPath,
+        imgName
       });
 
       newUser.save()
@@ -68,6 +76,34 @@ router.post('/login', passport.authenticate('local', {
   failureRedirect : '/login',
   failureFlash : true
 }));
+
+const ensureLogin = require("connect-ensure-login");
+
+
+router.get("/posts/new", ensureLogin.ensureLoggedIn('/login'), (req, res) => {
+  res.render("posts/new", { user: req.user });
+});
+
+router.post('/posts/new', uploadCloud.single('photo'), (req, res, next) => {
+  const { content, creatorId } = req.body;
+  const picPath = req.file.url;
+  const picName = req.file.originalname;
+  
+  const newPost = new Post({content, creatorId, picPath, picName});
+  
+  newPost.save()
+    .then(post => {
+      res.redirect('/');
+    })
+    .catch(error => {
+      next(error);
+    })
+  ;
+});
+
+router.get("/posts", ensureLogin.ensureLoggedIn('/login'), (req, res) => {
+  res.render("index", { user: req.user });
+});
 
 router.get('/logout', (req, res) => {
     req.logout();
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const Post = require('../models/post.js');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'Express - Generated with IronGenerator',
-    user: req.user
-  });
+  Post.find()
+    .then((posts) => {
+      res.render('index', { 
+        posts : posts,
+        user: req.user
+      });
+    })
+    .catch((error) => next(error))
+  ;
 });
 
 module.exports = router;
